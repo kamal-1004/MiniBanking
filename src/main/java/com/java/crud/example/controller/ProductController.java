2e7197733b7f30142d62cef0e39b863bc8f394e4
kamal-1004 <86104805+kamal-1004@users.noreply.github.com>
2022-12-15 01:51:47 +0530

    first complete version spring restful file
    first complete version spring restful file

---
+++ src/main/java/com/java/crud/example/controller/ProductController.java
@@ -0,0 +1,64 @@
+package com.java.crud.example.controller;
+
+import java.util.List;
+
+import org.hibernate.query.NativeQuery.ReturnableResultNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.java.crud.example.entity.Product;
+import com.java.crud.example.service.ProductService;
+
+@RestController
+public class ProductController {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping("/product")
+	public Product getDetails(){
+		return new Product(1, "ankush", 10, 110.00);
+	}
+	
+	@PostMapping("/addproduct")
+	public Product addProduct(@RequestBody Product product) {
+		return service.saveProduct(product);
+	}
+	
+	@PostMapping("/addproducts")
+	public List<Product> addProducts(@RequestBody List<Product> products){
+		return service.saveProducts(products);
+	}
+	
+	@GetMapping("/products")
+	public List<Product> findAllProducts(){
+		return service.getProducts();
+	}
+	
+	@GetMapping("/products/{id}")
+	public Product findProductById(@PathVariable int id) {
+		return service.getProductByID(id);
+	}
+	
+	@GetMapping("/products/{name}")
+	public Product findProductByName(@PathVariable String name) {
+		return service.getProductByName(name);
+	}
+
+	@PutMapping("/update")
+	public Product updateProduct(@RequestBody Product product) {
+		return service.updateProduct(product);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable int id) {
+		return service.deleteProduct(id);
+	}
+}
